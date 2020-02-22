84839fddeb67eefc35372355fb59839e8fbb5119
Aashish Gaikwad <aashish.gaikwad@steerlean.com>
2020-02-22 20:43:42 +0530

    Updates

---
+++ precache-manifest.e9d4bd1dcff3d877ead43344db0f7daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1065a0e4acbd4bf3c467108a60364db1",
+    "url": "/BlogDemoApp/index.html"
+  },
+  {
+    "revision": "8c681680c75dc4aeaf52",
+    "url": "/BlogDemoApp/static/css/main.c21eb626.chunk.css"
+  },
+  {
+    "revision": "c1f3a412e991b1a78ad4",
+    "url": "/BlogDemoApp/static/js/2.47a3d489.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BlogDemoApp/static/js/2.47a3d489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c681680c75dc4aeaf52",
+    "url": "/BlogDemoApp/static/js/main.3c6e31aa.chunk.js"
+  },
+  {
+    "revision": "946edc5a2ee865e13802",
+    "url": "/BlogDemoApp/static/js/runtime-main.de4b0afe.js"
+  }
+]);
