512bb6c814f777860b9d583703d2508003c73e67
Aashish Gaikwad <aashish.gaikwad@steerlean.com>
2020-02-22 21:39:25 +0530

    Changes in AppRouter file.

--- src/__tests__/NotFoundPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import NotFoundPage from '../components/NotFoundPage';
-
-test('renders Header correctly', () => {
-    const wrapper = shallow(<NotFoundPage />);
-    expect(toJson(wrapper)).toMatchSnapshot();
-});
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import ListPosts from '../components/ListPosts';
-import NotFoundPage from '../components/NotFoundPage';
 import ViewPost from '../components/ViewPost';
 import AddNewPost from '../components/AddNewPost';
 import EditPost from '../components/EditPost';
 
 const AppRouter = () => (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={ListPosts} />
-                <Route path="/post/:id" component={ViewPost} />
-                <Route path="/addPost" component={AddNewPost} />
-                <Route path="/editPost/:id" component={EditPost} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
+	<BrowserRouter>
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={ListPosts} />
+				<Route exact path="/post/:id" component={ViewPost} />
+				<Route exact path="/addPost" component={AddNewPost} />
+				<Route exact path="/editPost/:id" component={EditPost} />
+				<Redirect to="/" />
+			</Switch>
+		</div>
+	</BrowserRouter>
 );
 
 export default AppRouter;
