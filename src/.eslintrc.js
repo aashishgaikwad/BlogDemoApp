efa48b7c10abd14014a8b0a7261d4d0be43b083c
Aashish Gaikwad <aashish.gaikwad@steerlean.com>
2020-02-22 19:48:33 +0530

    Fixed git CI pipeline failing issue.Some minor changes.

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-	extends: [ 'react-app', 'plugin:jsx-a11y/recommended' ],
-	plugins: [ 'jsx-a11y' ],
-	rules: {
-		'no-console': 'error'
-	}
+    "env": {
+        "browser": true,
+        "jest": true,
+        "es6": true
+    },
+    "plugins": ["import", "jsx-a11y"],
+    "extends": ['react-app', 'plugin:jsx-a11y/recommended', 'eslint:recommended'],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": "warn",
+        "no-eval": "error",
+        "import/first": "error"
+    }
 };
